015a3abdab145d32bb93f3f1642af8f79b65e6ab
brian <info@sanctus-tech.com>
2022-03-30 00:24:32 +0200

    Fix dynamic cover and about text, and prepopulate them the right way (via fixture)

--- smartestate/views.py
+++ smartestate/views.py
@@ -5,8 +5,18 @@ from config.models import Config
 def index(request):
     site_title = Config.objects.get_or_create(
         config_var="site_title")[0].config_val
-    return render(request, 'index.html', {"site_title": site_title})
+    cover_text = Config.objects.get(config_var="cover_text").config_val
+    return render(request, 'index.html', {
+        "site_title": site_title,
+        "cover_text": cover_text,
+        }
+    )
 def about(request):
     site_title = Config.objects.get_or_create(
         config_var="site_title")[0].config_val
-    return render(request, 'index.html', {"site_title": site_title})
+    about_text = Config.objects.get(config_var="about_text").config_val
+    return render(request, 'about.html', {
+        "site_title": site_title,
+        "about_text": about_text,
+        }
+    )
