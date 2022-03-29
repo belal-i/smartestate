a986b6b55c3aa027ea579f52ae465eb6306bd44d
brian <info@sanctus-tech.com>
2022-03-29 02:30:00 +0200

    Make site title, cover text, and about text dynamic, edit in admin, and populate with default values

--- config/models.py
+++ config/models.py
@@ -7,8 +7,9 @@ from django.db import models
 #################
 
 class Config(models.Model):
-    config_var = models.CharField(max_length=16, default='site_title')
-    config_val = models.CharField(max_length=32, default='SmartEstate')
+    config_var = models.CharField(max_length=16, default='did_init')
+    config_val = models.CharField(max_length=2048, default='no')
     def __str__(self):
         return self.config_var
+
 
