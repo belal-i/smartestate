de548afbdede570498d2f3268185da009338f14a
brian <info@sanctus-tech.com>
2022-03-29 23:53:03 +0200

    Revert "Make site title, cover text, and about text dynamic, edit in admin, and populate with default values"
    This reverts commit a986b6b55c3aa027ea579f52ae465eb6306bd44d.

--- smartestate/views.py
+++ smartestate/views.py
@@ -1,26 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from config.models import Config
-from config.populate import populate
-
-# TODO: A more Django native way to do this?
-populate()
 
 def index(request):
-    site_title = Config.objects.get(config_var="site_title").config_val
-    cover_text = Config.objects.get(config_var="cover_text").config_val
-    return render(request, 'index.html', {
-        "site_title": site_title,
-        "cover_text": cover_text,
-        }
-    )
+    site_title = Config.objects.get_or_create(
+        config_var="site_title")[0].config_val
+    return render(request, 'index.html', {"site_title": site_title})
 def about(request):
-    site_title = Config.objects.get(config_var="site_title").config_val
-    # TODO: In terms of type safety, it might be better to do it like this,
-    # with .filter instead of .get
-    about_text = Config.objects.filter(config_var="about_text")[0].config_val
-    return render(request, 'about.html', {
-        "site_title": site_title,
-        "about_text": about_text,
-        }
-    )
+    site_title = Config.objects.get_or_create(
+        config_var="site_title")[0].config_val
+    return render(request, 'index.html', {"site_title": site_title})
