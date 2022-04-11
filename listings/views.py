69ddaebe3be79a62ab2e751ccf192046e1a76e10
brian <info@sanctus-tech.com>
2022-04-12 01:10:30 +0200

    Refactor the templates to make use of template inheritance (and rename index templates to something more appropriate, i.e. home, list)

--- listings/views.py
+++ listings/views.py
@@ -5,7 +5,7 @@ from .models import *
 from config.models import Config
 # Create your views here.
 
-def index(request):
+def list(request):
     site_title = Config.objects.filter(
         config_var="site_title")[0].config_val
     rental_listings = Listing.objects.filter(listing_type='rental')[:5]
@@ -15,7 +15,7 @@ def index(request):
         'for_sale_listings': for_sale_listings,
         'site_title': site_title,
     }
-    return render(request, 'listings/index.html', context)
+    return render(request, 'listings/list.html', context)
 
 def detail(request, listing_id):
     site_title = Config.objects.filter(
--- smartestate/views.py
+++ smartestate/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from config.models import Config
 
-def index(request):
+def home(request):
     site_title = Config.objects.get_or_create(
         config_var="site_title")[0].config_val
     cover_text = Config.objects.get(config_var="cover_text").config_val
-    return render(request, 'index.html', {
+    return render(request, 'smartestate/home.html', {
         "site_title": site_title,
         "cover_text": cover_text,
         }
@@ -15,7 +15,7 @@ def about(request):
     site_title = Config.objects.get_or_create(
         config_var="site_title")[0].config_val
     about_text = Config.objects.get(config_var="about_text").config_val
-    return render(request, 'about.html', {
+    return render(request, 'smartestate/about.html', {
         "site_title": site_title,
         "about_text": about_text,
         }
