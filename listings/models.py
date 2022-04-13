5c295cbb38df0da080b6e82e9279a945e95486e8
brian <info@sanctus-tech.com>
2022-04-13 22:41:41 +0200

    Fix import statement style

--- listings/models.py
+++ listings/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator, \
+from django.core.validators import (
+    MinValueValidator, MaxValueValidator,
     validate_comma_separated_integer_list
+)
 
 from datetime import date
 
