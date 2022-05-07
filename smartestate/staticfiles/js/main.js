03ad702f59a657ee416c59ab3610a1aa8e186297
brian <info@sanctus-tech.com>
2022-05-08 01:46:00 +0200

    Provide an icon for toggling between day and night mode, toggle the 'src' attribute of that icon depending on the current day or night value as well

--- smartestate/staticfiles/js/main.js
+++ smartestate/staticfiles/js/main.js
@@ -14,10 +14,14 @@ $(document).ready(function() {
 		$("h2").toggleClass("text-dark");
 		$("p").toggleClass("text-dark");
 		$("span").toggleClass("text-dark");
+		var dayNightIconPath = $("#toggle-day-night-icon").attr("src");
+		dayNightIconPath = dayNightIconPath.replace("night", "day");
+		$("#toggle-day-night-icon").attr("src", dayNightIconPath);
 	}
 });
 $("#toggle-day-night").click(function() {
 	var mode;
+	var dayNightIconPath = $("#toggle-day-night-icon").attr("src");
 	if(Cookies.get("mode") == undefined) {
 		mode = 1;
 		Cookies.set("mode", mode, {sameSite: "strict"});
@@ -37,6 +41,12 @@ $("#toggle-day-night").click(function() {
 	$("h2").toggleClass("text-dark");
 	$("p").toggleClass("text-dark");
 	$("span").toggleClass("text-dark");
+	if(mode == 1) {
+		dayNightIconPath = dayNightIconPath.replace("night", "day");
+	} else {
+		dayNightIconPath = dayNightIconPath.replace("day", "night");
+	}
+	$("#toggle-day-night-icon").attr("src", dayNightIconPath);
 	mode *= -1;
 	Cookies.set("mode", mode, {sameSite: "strict"});
 });
