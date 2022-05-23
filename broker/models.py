28cd5c1940cea043152819ccc6f2eeabda9b7f03
brian <info@sanctus-tech.com>
2022-05-23 18:47:41 +0200

    Implement the Seeking model, for people looking for a rental or for-sale property

--- broker/models.py
+++ broker/models.py
@@ -1,4 +1,8 @@
+from datetime import date
+
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 
@@ -11,6 +15,7 @@ class Contact(models.Model):
     phone = models.ManyToManyField('Phone', blank=True)
     email = models.ManyToManyField('Email', blank=True)
     company = models.ManyToManyField('Company', blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
     def __str__(self):
         return self.first_name + " " + self.last_name
 
@@ -45,3 +50,45 @@ class Company(models.Model):
     address = models.ManyToManyField('Address', blank=True)
     def __str__(self):
         return self.name
+
+class Seeking(models.Model):
+    SEEKING_TYPE_CHOICES = (
+        ('rental','RENTAL'),
+        ('for_sale','FOR_SALE'),
+    )
+    seeking_type = models.CharField(max_length=8,
+        choices=SEEKING_TYPE_CHOICES, default='rental')
+    # TODO: Make this ManyToMany?
+    # TODO: Perhaps implement a seeking_contact model, with some
+    #       seeking-related info, and that has a relation to the 
+    #       "plain" contact model.
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, 
+        null=True, blank=True)
+    max_rent = models.DecimalField(null=True, blank=True,
+        max_digits=7, decimal_places=2)
+    max_purchase_price = models.DecimalField(null=True, blank=True,
+        max_digits=10, decimal_places=2)
+    min_number_of_rooms = models.IntegerField(null=True, blank=True, default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(100)])
+    min_size_qm = models.IntegerField(null=True, blank=True, default=20,
+        validators=[MinValueValidator(1), MaxValueValidator(500)])
+    number_of_persons = models.IntegerField(default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(12)])
+
+    # TODO: Implement some automatic synchronization between these fields####
+    starting_date = models.DateField(null=True, blank=True, default=date.today)
+    ending_date = models.DateField(null=True, blank=True)
+    number_of_months = models.IntegerField(null=True, blank=True)
+    ####
+
+    must_be_furnished = models.BooleanField(default=False)
+    line_of_work = models.CharField(null=True, blank=True, max_length=64,
+        help_text="Type of job that this tenant/buyer has")
+    employer = models.ForeignKey(Company, on_delete=models.CASCADE,
+        null=True, blank=True)
+    is_smoker = models.BooleanField(default=False)
+    has_pets = models.BooleanField(default=False)
+    notes = models.TextField(max_length=2048, null=True, blank=True)
+    contact_source = models.CharField(max_length=64, null=True, blank=True,
+        help_text="How did this person find out about us?")
+
