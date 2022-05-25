79cea868bdf6c85b74217e490e627858b4879f96
brian <info@sanctus-tech.com>
2022-05-25 12:38:36 +0200

    Implement some basic list views for the broker app

--- broker/urls.py
+++ broker/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'broker'
+
 urlpatterns = [
-    path('', views.test, name='test'),
+    path('', views.start, name='start'),
+    path('listings/', views.listings, name='listings'),
+    path('seekings/', views.seekings, name='seekings'),
+    path('matchings/', views.matchings, name='matchings'),
 ]
--- broker/views.py
+++ broker/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import *
+from .models import *
+
 # Create your views here.
 
-def test(request):
-    return HttpResponse("test")
+def start(request):
+    listings = Listing.objects.all()[:10]
+    seekings = Seeking.objects.all()[:10]
+    matchings = Matching.objects.all()[:10]
+    context = {
+        'listings': listings,
+        'seekings': seekings,
+        'matchings': matchings,
+    }
+    return render(request, 'broker/start.html', context)
+def listings(request):
+    listings = Listing.objects.all()
+    return render(request, 'broker/listings.html', {'listings': listings})
+def seekings(request):
+    seekings = Seeking.objects.all()
+    return render(request, 'broker/seekings.html', {'seekings': seekings})
+def matchings(request):
+    matchings = Matching.objects.all()
+    return render(request, 'broker/matchings.html', {'matchings': matchings})
