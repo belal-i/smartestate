9015e102e20e11a813140cb77b8ad445863b3f29
brian <brian.st-hilaire@sanctus-tech.com>
2022-06-17 00:54:02 +0200

    Wire the search form on homepage into the search results view

--- listings/views.py
+++ listings/views.py
@@ -4,7 +4,7 @@ from django.db.models import Q
 
 from .models import *
 from config.models import Config
-from broker.utils import keyword_search_listing
+from broker.utils import keyword_search_listing, filter_search_listing
 # Create your views here.
 
 def list_rental(request):
@@ -36,9 +36,79 @@ def detail(request, listing_id):
 
 def search_results(request):
 
-    search_results = keyword_search_listing(request.GET['search'])
+    if request.GET.get('search', '') != '':
+        search_results = keyword_search_listing(request.GET['search'])
+    else:
+        search_results = filter_search_listing(
+            validate_search_params(request.GET)
+        )
+
 
     context = {
         'search_results': search_results,
     }
     return render(request, 'listings/search-results.html', context)
+
+def validate_search_params(input_dict):
+    return_dict = input_dict.copy().dict()
+    str_fields = [
+        'listing_type',
+        'earliest_date_available',
+        'latest_date_available',
+        'max_date_of_construction',
+        'min_date_of_construction',
+    ]
+    bool_fields = [
+        'pets_ok',
+        'is_primary',
+        'has_internet',
+        'is_furnished',
+    ]
+    int_fields = [
+        'minimum_months',
+        'maximum_months',
+        'max_number_of_people',
+        'min_number_of_people',
+        'max_number_of_rooms',
+        'min_number_of_rooms',
+        'max_size_sq_m',
+        'min_size_sq_m',
+    ]
+    float_fields = [
+        'max_rental_price',
+        'min_rental_price',
+        'max_security_deposit',
+        'min_security_deposit',
+        'max_for_sale_price',
+        'min_for_sale_price',
+        'min_minimum_down_payment',
+        'max_minimum_down_payment',
+    ]
+    for field in str_fields:
+        try:
+            if return_dict[field] == '':
+                return_dict.pop(field)
+        except KeyError:
+            pass
+    for field in bool_fields:
+        try:
+            return_dict[field] = bool(return_dict[field])
+        except KeyError:
+            pass
+        except ValueError:
+            return_dict.pop(field)
+    for field in int_fields:
+        try:
+            return_dict[field] = int(return_dict[field])
+        except KeyError:
+            pass
+        except ValueError:
+            return_dict.pop(field)
+    for field in float_fields:
+        try:
+            return_dict[field] = float(return_dict[field])
+        except KeyError:
+            pass
+        except ValueError:
+            return_dict.pop(field)
+    return return_dict
