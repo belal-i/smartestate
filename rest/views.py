4f04508370eaa10dc3c728070db6d571858ef00b
brian <brian.st-hilaire@sanctus-tech.com>
2022-07-10 02:42:37 +0200

    POST a matching by sending listing_id and seeking_id in the data

--- rest/views.py
+++ rest/views.py
@@ -1,13 +1,10 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
 
-from listings.models import *
-from broker.models import *
-from broker.utils import *
 
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.authentication import (
     SessionAuthentication,
     BasicAuthentication,
@@ -23,8 +20,9 @@ from rest_framework.decorators import (
     permission_classes,
 )
 
-
-
+from listings.models import *
+from broker.models import *
+from broker.utils import *
 from .serializers import *
 
 """
@@ -53,7 +51,7 @@ def seekings(request):
         return JsonResponse(serializer.data, safe=False)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def matchings(request):
@@ -61,3 +59,30 @@ def matchings(request):
         matchings = Matching.objects.all()
         serializer = MatchingSerializer(matchings, many=True)
         return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        """
+        To debug this, send a get request to server, get the CSRF token from
+        that request, then paste it into a new  Header parameter called X-CSRFToken
+        """
+        listing_id = request.data.get('listing_id', '1')
+        seeking_id = request.data.get('seeking_id', '1')
+        error_occurred = False
+        try:
+            listing = Listing.objects.get(pk=listing_id)
+            seeking = Seeking.objects.get(pk=seeking_id)
+        except ObjectDoesNotExist:
+            error_occurred = True
+            error_response = {
+                'status': 'Bad request',
+                'message': 'Either the specified seeking or listing does not exist.',
+            }
+
+        if error_occurred:
+            return JsonResponse(error_response,
+                status=status.HTTP_400_BAD_REQUEST)
+        else:
+            matching = Matching(listing=listing, seeking=seeking)
+            matching.save()
+
+            serializer = MatchingSerializer(matching)
+            return JsonResponse(serializer.data)
